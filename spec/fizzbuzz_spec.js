17c5c47feafea6ae0a52941b82b6343ea9e8b405
Kent Korgan <korgankd@dukes.jmu.edu>
2016-03-02 21:57:33 -0500

    one spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failuresone spec no failures

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -3,7 +3,8 @@ var fb = require('../fizzbuzz');
 describe("A fizzbuzz program", function() {
 
 	it("can count from 1 to 100", function() {
-		expect(fb.count(1, 10)).tobe("1 2 fizz 4 buzz fizz 7 8 fizz buzz");
+		expect(fb.count(1, 10)).toBe("1 2 fizz 4 buzz fizz 7 8 fizz buzz ");
+		expect(fb.count(1, 5)).toBe("1 2 fizz 4 buzz ");
 	});
 
 });
